ccf00624f7d7db2542628444a4807faf15bc0cff
Fabbri <theofabbri@yahoo.fr>
2020-06-10 09:08:08 +0200

    début

--- steganographie.py
+++ steganographie.py
@@ -1,3 +1,18 @@
-import Image
+from PIL import Image
 
-im = Image.open
+
+im = Image.open("python_img.png")
+
+width, height = im.size
+
+r, g, b, a = im.split()
+
+redList = list(r.getdata())
+greenList = list(g.getdata())
+blueList = list(b.getdata())
+
+print(redList)
+print(greenList)
+print(blueList)
+
+#im.show()
